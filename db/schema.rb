1f5f5b09dbc02f58e02a04b355a250581f2a2d37
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2016-03-11 19:07:05 +0530

    Changes in schema done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216080831) do
+ActiveRecord::Schema.define(version: 20160218070614) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "link_id"
+    t.text     "body"
+    t.integer  "user_id"
+  end
+
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
